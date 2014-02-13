ef02b3c06756b5ca3a99e9cb89624f3fcec7d0a9
Lucas Bernardi <lbernardi@despegar.com>
2014-02-13 20:15:26 -0300

    better simulation

--- src/main/java/Regalator.java
+++ src/main/java/Regalator.java
@@ -1,5 +1,8 @@
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Random;
+
+import org.apache.commons.lang3.RandomStringUtils;
 
 public class Regalator {
 
@@ -10,16 +13,21 @@ public class Regalator {
 				.parseMLCategories("all");
 
 		JaccardIndex distanceMeasure = new JaccardIndex();
-		for (int i = 0; i < allMlCategories.size(); i++) {
-			MLCategory x = allMlCategories.get(i);
+		Random r = new Random();
+		for (int i = 0; i < 1000; i++) {
+			MLCategory x = allMlCategories
+					.get(r.nextInt(allMlCategories.size()));
+			System.out.println(x);
 			for (int j = 0; j < allMlCategories.size(); j++) {
 				MLCategory y = allMlCategories.get(j);
 				double distance = distanceMeasure.computeDistance(x, y);
 				if (distance >= 0.5) {
-					System.out.println(x + "  " + y + ": " + distance);
+					System.out.println(y + ": " + distance);
 				}
 			}
-
+			System.out.println("------------------------------------------");
+			System.out.println("------------------------------------------");
+			System.out.println("------------------------------------------");
 		}
 
 	}
