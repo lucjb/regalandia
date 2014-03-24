5098be0415c18f2acb4e5854a6bafc663ebe3ce5
cronopio <lucejb@gmail.com>
2014-03-24 15:50:46 -0300

    changed getTitle, no it whows the whole path

--- src/main/java/org/cronopios/regalator/AbstractCanonicalCategory.java
+++ src/main/java/org/cronopios/regalator/AbstractCanonicalCategory.java
@@ -10,11 +10,7 @@ public abstract class AbstractCanonicalCategory implements CanonicalCategory {
 
 	@Override
 	public String toString() {
-		StringBuilder ts = new StringBuilder();
-		for (CanonicalCategory ancestor : this.getPathFromRoot()) {
-			ts.append(ancestor.getName()).append(">");
-		}
-		return ts.toString();
+		return this.getTitle();
 	}
 
 	@Override
@@ -40,8 +36,7 @@ public abstract class AbstractCanonicalCategory implements CanonicalCategory {
 		if (this.getName().equals(categoryPathElementSubstring))
 			return true;
 		if (this.getPathFromRoot() != null) {
-			List<? extends CanonicalCategory> path_from_root2 = this
-					.getPathFromRoot();
+			List<? extends CanonicalCategory> path_from_root2 = this.getPathFromRoot();
 			for (CanonicalCategory cat : path_from_root2) {
 				if (cat != this && cat.isFor(categoryPathElementSubstring)) {
 					return true;
@@ -85,7 +80,11 @@ public abstract class AbstractCanonicalCategory implements CanonicalCategory {
 
 	@Override
 	public String getTitle() {
-		return this.getName();
+		StringBuilder ts = new StringBuilder();
+		for (CanonicalCategory ancestor : this.getPathFromRoot()) {
+			ts.append(ancestor.getName()).append(">");
+		}
+		return ts.toString();
 	}
 
 }
